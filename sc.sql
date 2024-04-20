10742e8ae0df7de376ac3a89460090fc89252e74
aminulhoquenusu <nusunusu111@gmail.com>
2024-04-20 18:48:46 +0600

    view

--- sc.sql
+++ sc.sql
@@ -19,7 +19,7 @@ select * from student;
 commit;
 
 
-
+CREATE TABLE 
 
 
 
---
+++ view.sql
@@ -0,0 +1,9 @@
+create view view_emp_100_days
+as
+select distinct(job_id)
+from employees
+where current_date- hire_date > 100
+group by job_id
+having count(employee_id) > 3;
+
+select * from view_emp_100_days;
